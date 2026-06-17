fbee688b1acb43e72060dd5ecef5e57acb80d32b
SpideErik <erikozh@gmail.com>
2026-06-17 10:12:44 +0300

    add: main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+def show_project_title():
+    print("Git practice project started")
+
+
+def show_student_info():
+    student_name = "Кожемякин Эрик"
+    group = "РПО/1"
+    print(f"Student: {student_name}")
+    print(f"Group: {group}")
+
+
+def main():
+    show_project_title()
+    show_student_info()
+
+
+if __name__ == "__main__":
+    main()
